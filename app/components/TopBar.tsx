ae8ef905c4d5232ae19f40ab097819b8d57005da
Eric Clemens <clemens72@gmail.com>
2025-02-21 12:58:20 -0500

    updated topbar aesthetics and removed modeswitcher

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -3,7 +3,6 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Image from 'next/image';
@@ -13,23 +12,21 @@ export default function TopBar() {
   return (
       <AppBar position="fixed">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Box sx={{ marginRight: 2, alignItems: 'center', display: 'flex' }}>
             <Image src={logoCA} alt="Class Acts Logo" height={25} />
           </Box>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'white' }}>
             Class Acts Entertainment
           </Typography>
-          <Button color="inherit">Log In</Button>
-          <Button color="inherit">Sign Up</Button>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ ml: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
   );
