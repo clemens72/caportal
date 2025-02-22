29c7c415c0e665c7453afc211523fe3f3552608b
Eric Clemens <clemens72@gmail.com>
2025-02-21 19:23:20 -0500

    toolpad midintegration and google auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,32 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
 import TopBar from './components/TopBar';
+import TimeProvider from './components/Providers/TimeProvider';
 import { SessionProvider, signIn, signOut } from "next-auth/react";
+import { AppProvider, Navigation } from './components/Providers/AppProvider';
+import HomeIcon from "@mui/icons-material/Home";
 import { auth } from '../auth';
+
+const NAVIGATION: Navigation = [
+  {
+    kind: "header",
+    title: "Main Menu",
+  },
+  {
+    segment: "",
+    title: "Home",
+    icon: <HomeIcon />,
+  },
+];
+
+const BRANDING = {
+  title: 'Class Acts Entertainment',
+};
+
+const AUTHENTICATION = {
+  signIn,
+  signOut,
+};
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const session = await auth();
@@ -13,15 +37,25 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-      <SessionProvider session={session}>
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <ThemeProvider theme={theme}>
-            <TopBar />
-            <CssBaseline />
-            {props.children}
-          </ThemeProvider>
-        </AppRouterCacheProvider>
-      </SessionProvider>
+        <TimeProvider>
+          <SessionProvider session={session}>
+            <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+              <ThemeProvider theme={theme}>
+                <TopBar />
+                <CssBaseline />
+                <AppProvider
+                navigation={NAVIGATION}
+                branding={BRANDING}
+                session={session}
+                authentication={AUTHENTICATION}
+                theme={theme}
+              >
+                {props.children}
+                </AppProvider>
+              </ThemeProvider>
+            </AppRouterCacheProvider>
+          </SessionProvider>
+        </TimeProvider>
       </body>
     </html>
   );
